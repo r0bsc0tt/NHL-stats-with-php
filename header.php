9761b8e4a190dde456417a359dd2c8b0ea977ea4
Rob Scott <r0bsc0tt@users.noreply.github.com>
2016-04-06 16:18:49 -0500

    Update header.php
    set path to project variable

--- header.php
+++ header.php
@@ -3,7 +3,7 @@ function get_head($pagetitle){ ?>
 <!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
-  <?php $pathtoproject = 'http://162.246.57.247/~newdev/nhl-stats/'; ?>
+  <?php $pathtoproject = 'http://pathToProject.com/'; ?>
   <link rel="stylesheet" type="text/css" href="<?php echo $pathtoproject?>css/style.css" />
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>
